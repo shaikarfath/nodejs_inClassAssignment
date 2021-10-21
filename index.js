24dc07a1331700471e9beda0b9830e1719766937
shaikarfath <shaik_arfath@columbusstate.edu>
2021-10-21 20:07:50 +0000

    node ejs

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
- var express = require('express');
- var app= express();
+var express = require('express');
+var bodyParser = require('body-parser');
 
- app.get('/', function(request, response){
-    response.send('Hello World!');
- });
+var app= express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get('/', function(request, response){
+    response.render('index');
+    //response.send('Hello World!');
+});
+
+app.post('/addToDo', function(req, res){
+    res.send('hello');
+})
+
+app.post('/removeToDo', function(req,res){
+    res.send('world');
+})
 
 app.listen(3000, function(){
     console.log('App is running on port 3000!')
