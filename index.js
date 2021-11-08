99c7a726d854c11d55293c27ba73e76652d8f447
shaikarfath <shaik_arfath@columbusstate.edu>
2021-11-08 16:14:09 +0000

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var tasks = [];
+var tasks = ['wake up', 'eat breakfast'];
 var completed = [];
 
 var app= express();
@@ -9,15 +9,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(request, response){
-    response.render('index', {tasks:tasks , completed: completed});
+    response.render('index', {tasks: tasks , completed: completed});
     //response.send('Hello World!');
 });
 
 app.post('/addToDo', function(req, res){
-    res.send('hello');
+    
+    tasks.push(req.body.newtodo)
+    res.redirect('/');
 })
 
 app.post('/removeToDo', function(req,res){
+    const remove = req.body.check;
+  
+    if(typeof remove === 'string'){
+        tasks.splice( tasks.indexOf(remove),1)
+    } else if(typeof remove === "object"){
+        for(var i=0; i<remove.length; i++){
+            tasks.splice( tasks.indexOf(remove[i]),1)
+        }
+    }
+    res.redirect('/')
+})
+
+app.post('/deleteToDo', function(req,res){
     res.send('world');
 })
 
